614a7de3a212d8b6ceb24b7babd4246d44144a56
Tomáš Kolinger <tomas@kolinger.name>
2018-10-28 13:13:40 +0100

    added secret key for flask

--- web.py
+++ web.py
@@ -1,10 +1,12 @@
 import logging
 from logging import StreamHandler
+import secrets
 import sys
 
 from flask import Flask
 import socketio
 
+from utils.config import Config
 from utils.storage import Storage
 from webapp.backend import Backend
 from webapp.index import Index
@@ -15,6 +17,13 @@ if len(sys.argv) > 1:
 
 app = Flask(__name__)
 app.register_blueprint(Index().register())
+
+config = Config()
+secret_key = config.read("secret_key")
+if not secret_key:
+    secret_key = secrets.token_hex(16)
+    config.write("secret_key", secret_key)
+app.secret_key = secret_key
 
 sockets = socketio.Server(async_mode="threading")
 app.wsgi_app = socketio.Middleware(sockets, app.wsgi_app)
