fd581a4988ad651836312ebe20140f6a22e743cf
Tomáš Kolinger <tomas@kolinger.name>
2024-02-12 09:51:52 +0100

    updated build script

--- docker-build-all-push.cmd
+++ docker-build-all-push.cmd
@@ -3,7 +3,6 @@ python utils/version.py version.txt || goto :error
 set /p VERSION=<version.txt
 
 docker buildx build --push --builder=builder --platform linux/arm/v7,linux/arm64/v8,linux/amd64 -t kolinger/rd-usb:%VERSION% -t kolinger/rd-usb:latest . || goto :error
-docker buildx stop builder
 
 goto :end
 
